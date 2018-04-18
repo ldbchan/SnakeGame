e0820526693e5badb1c63af15db7fac22a4fb2c9
ldbchan <ldb.chan@gmail.com>
2018-04-18 17:08:46 +0800

    Add SGSnake.h/m

---
+++ SnakeGame/Model/SGSnake.h
@@ -0,0 +1,13 @@
+//
+//  SGSnake.h
+//  SnakeGame
+//
+//  Created by chantil on 04/18/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGSnake : NSObject
+
+@end
---
+++ SnakeGame/Model/SGSnake.m
@@ -0,0 +1,13 @@
+//
+//  SGSnake.m
+//  SnakeGame
+//
+//  Created by chantil on 04/18/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import "SGSnake.h"
+
+@implementation SGSnake
+
+@end
