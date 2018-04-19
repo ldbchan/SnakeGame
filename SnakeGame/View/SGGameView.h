ef386b0f933e8fa7776ee08eeccd8c8a506419d4
ldbchan <ldb.chan@gmail.com>
2018-04-19 15:53:36 +0800

    Add SGGameView.h/m

---
+++ SnakeGame/View/SGGameView.h
@@ -0,0 +1,13 @@
+//
+//  SGGameView.h
+//  SnakeGame
+//
+//  Created by chantil on 04/19/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGGameView : UIView
+
+@end
---
+++ SnakeGame/View/SGGameView.m
@@ -0,0 +1,21 @@
+//
+//  SGGameView.m
+//  SnakeGame
+//
+//  Created by chantil on 04/19/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import "SGGameView.h"
+
+@implementation SGGameView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
