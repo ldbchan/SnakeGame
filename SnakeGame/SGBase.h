8b387c150bde6e1b1cc78036b8a5761f30c77709
ldbchan <ldb.chan@gmail.com>
2018-04-20 10:20:04 -0700

    Add SGBase.h/m

---
+++ SnakeGame/SGBase.h
@@ -0,0 +1,13 @@
+//
+//  SGBase.h
+//  SnakeGame
+//
+//  Created by chantil on 04/20/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGBase : NSObject
+
+@end
---
+++ SnakeGame/SGBase.m
@@ -0,0 +1,13 @@
+//
+//  SGBase.m
+//  SnakeGame
+//
+//  Created by chantil on 04/20/2018.
+//  Copyright © 2018 ldbchan. All rights reserved.
+//
+
+#import "SGBase.h"
+
+@implementation SGBase
+
+@end
