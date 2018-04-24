e1a12e4652dccc3bf163af3624b672d2ac2a465e
ldbchan <ldb.chan@gmail.com>
2018-04-24 15:14:23 -0700

    Implement SGGameView

--- SnakeGame/View/SGGameView.h
+++ SnakeGame/View/SGGameView.h
@@ -7,7 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SGSnake.h"
+
+@class SGGameView;
+
+#pragma mark - SGGameViewDataSource
+
+@protocol SGGameViewDataSource <NSObject>
+
+@required
+- (SGSnake *)snakeInGameView:(SGGameView *)gameView;
+- (SGPoint)foodPointInGameView:(SGGameView *)gameView;
+
+@end
+
+#pragma mark - SGGameViewDelegate
+
+@protocol SGGameViewDelegate <NSObject>
+
+@required
+- (void)gameViewDidSwipeLeft:(SGGameView *)gameView;
+- (void)gameViewDidSwipeRight:(SGGameView *)gameView;
+- (void)gameViewDidSwipeUp:(SGGameView *)gameView;
+- (void)gameViewDidSwipeDown:(SGGameView *)gameView;
+
+@end
+
+#pragma mark - SGGameView
 
 @interface SGGameView : UIView
+
+@property (nonatomic, weak) id<SGGameViewDataSource> dataSource;
+@property (nonatomic, weak) id<SGGameViewDelegate> delegate;
+@property (nonatomic, strong) SGSnake *snake;
+
+- (void)reload;
 
 @end
--- SnakeGame/View/SGGameView.m
+++ SnakeGame/View/SGGameView.m
@@ -10,12 +10,72 @@
 
 @implementation SGGameView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    SGSnake *snake = [self.dataSource snakeInGameView:self];
+    SGWorldSize worldSize = snake.worldSize;
+    CGSize actualSize = self.bounds.size;
+    CGFloat w = actualSize.width / worldSize.width;
+    CGFloat h = actualSize.height / worldSize.height;
+
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    // draw background
+    CGContextSetFillColorWithColor(context, UIColor.whiteColor.CGColor);
+    CGContextFillRect(context, rect);
+
+    // draw snake
+    CGContextSetFillColorWithColor(context, UIColor.grayColor.CGColor);
+
+    for (NSValue *value in snake.bodyPoints) {
+        SGPoint bodyPoint = [value SGPointValue];
+        CGContextSetFillColorWithColor(context, UIColor.grayColor.CGColor);
+        CGContextFillRect(context, CGRectMake(w * bodyPoint.x, h * bodyPoint.y, w, h));
+    }
+
+    // draw fruit
+    SGPoint foodPoint = [self.dataSource foodPointInGameView:self];
+    CGContextSetFillColorWithColor(context, UIColor.greenColor.CGColor);
+    CGContextFillEllipseInRect(context, CGRectMake(w * foodPoint.x, h * foodPoint.y, w, h));
+
+    // add swipe gesture recognizers once
+    if (!self.gestureRecognizers.count) {
+        NSArray *directions = @[@(UISwipeGestureRecognizerDirectionUp),
+                                @(UISwipeGestureRecognizerDirectionDown),
+                                @(UISwipeGestureRecognizerDirectionLeft),
+                                @(UISwipeGestureRecognizerDirectionRight)];
+
+        for (NSNumber *number in directions) {
+            UISwipeGestureRecognizerDirection direction = number.unsignedIntegerValue;
+            UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(didSwipe:)];
+            swipeRecognizer.direction = direction;
+            [self addGestureRecognizer:swipeRecognizer];
+        }
+    }
 }
-*/
+
+#pragma mark - Pulic Method
+
+- (void)reload {
+    [self setNeedsDisplay];
+}
+
+#pragma mark - Recoginzer
+
+- (void)didSwipe:(UISwipeGestureRecognizer *)swipeGestureRecognizer {
+    switch (swipeGestureRecognizer.direction) {
+        case UISwipeGestureRecognizerDirectionUp:
+            [self.delegate gameViewDidSwipeUp:self];
+            break;
+        case UISwipeGestureRecognizerDirectionDown:
+            [self.delegate gameViewDidSwipeDown:self];
+            break;
+        case UISwipeGestureRecognizerDirectionLeft:
+            [self.delegate gameViewDidSwipeLeft:self];
+            break;
+        case UISwipeGestureRecognizerDirectionRight:
+            [self.delegate gameViewDidSwipeRight:self];
+            break;
+    }
+}
 
 @end
